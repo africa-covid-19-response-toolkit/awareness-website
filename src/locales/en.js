59e857ae293fb08365671c391ab40da671cbffc9
Abel Beyene <contact.abeyene@gmail.com>
2020-04-08 10:57:38 +0100

    Added 404 page with links to other pages

--- src/locales/en.js
+++ src/locales/en.js
@@ -33,5 +33,8 @@ export default {
   sick:
     "A guide to help you make decisions and seek appropriate medical care. COVID-19 is the new respiratory disease spreading around the world and it is caused by a coronavirus. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   what:
-    "COVID-19 is the new respiratory disease spreading around the world and it is caused by a coronavirus. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    "COVID-19 is the new respiratory disease spreading around the world and it is caused by a coronavirus. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  page_not_found_title: "Page not found",
+  page_not_found_desc:
+    "Sorry, we couldn't find the page you are looking for. Try one of the following pages:"
 };
