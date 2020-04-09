9b5b074ed62725adb9206bbac95f626ae3484658
Tigist <tgd-oss@users.noreply.github.com>
2020-04-09 00:05:49 -0400

    Add text as Markdown

--- src/locales/en.js
+++ src/locales/en.js
@@ -6,7 +6,7 @@ export default {
   snippets: {
     what: "What is COVID-19?",
     what_detail:
-      "COVID-19 is the new respiratory disease spreading around the world and it is caused by a coronavirus.",
+      "COVID-19 is the a respiratory disease spreading around the world and it is caused by a coronavirus.",
     protection: "How to protect yourself",
     protection_detail:
       "There is currently no vaccine to prevent coronavirus disease 2009 (COVID-19).",
@@ -21,12 +21,163 @@ export default {
     public_report: "PUBLIC REPORT",
     volunteer_form: "VOLUNTEER FORM"
   },
-  care:
-    "Monitor the person for worsening symptoms, know the emergency warning signs. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  protection:
-    "There is currently no vaccine to prevent coronavirus disease 2009 (COVID-19). Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  sick:
-    "A guide to help you make decisions and seek appropriate medical care. COVID-19 is the new respiratory disease spreading around the world and it is caused by a coronavirus. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  what:
-    "COVID-19 is the new respiratory disease spreading around the world and it is caused by a coronavirus. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  what: `
+COVID-19 is a new respiratory disease spreading around the world whichand it is caused by a coronavirus that has not been previously identified in humans. COVID-19 is short for "Coronavirus Disease 2019."
+
+* The virus is thought to spread mainly between people who are in close contact with one another (about 2 meters) and through respiratory droplets produced when an infected person coughs or sneezes.
+* People are most contagious when they are the sickest. But those who don’t have a lot of symptoms can still pass the virus on to others.
+* There is currently no vaccine or cure for COVID-19 but researchers are working to find one.
+
+### WHAT ARE THE COVID-19 SYMPTOMS?
+
+Symptoms may appear 2 to 14 days after exposure and range from mild to severe illness.
+
+* Coughing
+* Shortness of breath
+* Sore throat
+* Headache
+* Fever
+
+### HOW IS THE COVID-19 COMPARED TO SEASONAL FLU?
+
+Both COVID-19 and seasonal flu cause fever, cough and aches. However, COVID-19 is much more severe than seasonal flu and no vaccine is available at this time whereas a vaccine is available to prevent some types of the flu. Someone with a COVID-19 virus can infect approximately 2.5 people, which is higher than seasonal flu.
+
+### HOW SEVERE IS COVID-19 INFECTION?
+
+There is a lot we don’t know about the COVID-19 infection. However, from the data we have, almost 80% of the infections appear asymptomatic or mild while 20% are severe and critical infections. So far, the mortality rate for COVID-19 is also higher than seasonal flu.
+
+### WHO IS AT HIGH RISK FOR COVID-19?
+
+Everyone is at risk of getting COVID-19, but some people are at a higher risk of serious illness.
+
+Generally, adults aged 65 and older and people of any age who have heart disease, lung disease, and diabetes with complications and obesity may be at higher risk for serious illness from COVID-19. However, it is possible for children and pregnant women to be infected, so they should also follow the proper prevention. There is not enough evidence to suggest that a mother who contracts COVID-19 will transmit the virus to her baby during pregnancy.
+
+### WHAT TO DO IF I AM FEELING ANXIOUS OR STRESSED BECAUSE OF EVENTS RELATED TO COVID-19?
+
+Take a break from social media and news. Do deep breaths exercises. Try doing simple stretching exercises at home. Keep talking to your family and friends and talk about how you are feeling  
+`,
+  care: `
+### MONITOR THE PERSON FOR WORSENING SYMPTOMS. KNOW THE EMERGENCY WARNING SIGNS.
+
+If they develop emergency warning signs for COVID-19, get medical attention immediately. Emergency warning signs include, but are not limited to:
+
+* Trouble breathing
+* Persistent pain or pressure in the chest
+* New confusion or inability to arouse
+* Bluish lips or face
+* Severe, constant dizziness or lightheadedness
+* Slurred speech
+
+If you develop emergency warning signs, call xxx.
+
+### PREVENT THE SPREAD OF GERMS WHEN CARING FOR SOMEONE WHO IS SICK
+* Have the person stay in one room, away from other people, including yourself, as much as possible.
+    * If possible, have them use a separate bathroom.
+    * Avoid sharing personal household items, like dishes, towels, and bedding
+    * If facemasks are available, have them wear a facemask when they are around people, including you.
+    * If the sick person can’t wear a facemask, you should wear one while in the same room with them, if facemasks are available. Here is how to make your own mask at home.
+    * If the sick person needs to be around others (within the home, in a vehicle, or doctor’s office), they should wear a facemask.
+* Wash your hands often with soap and water for at least 20 seconds, especially after interacting with the sick person. If soap and water are not readily available, use a hand sanitizer that contains at least 60% alcohol. Cover all surfaces of your hands and rub them together until they feel dry.
+* Avoid touching your eyes, nose, and mouth.
+* Every day, clean all surfaces that are touched often, like counters, tabletops, and doorknobs. Use household cleaning sprays or wipes according to the label instructions.
+* Wash laundry thoroughly. If laundry is soiled, wear disposable gloves and keep the soiled items away from your body while laundering. Wash your hands immediately after removing gloves.
+* Avoid having any unnecessary visitors.
+* For any additional questions about their care, contact their healthcare provider or state or local health department.
+
+### PROVIDE SYMPTOM TREATMENT
+
+* Make sure the sick person drinks a lot of fluids to stay hydrated and rests at home.
+* Over-the-counter medicines may help with symptoms.
+* For most people, symptoms last a few days and get better after a week.
+
+### WHEN TO END HOME ISOLATION (STAYING HOME)
+
+People with COVID-19 who have stayed home (are home isolated) can stop home isolation under the following conditions:
+
+* If they will not have a test to determine if they are still contagious, they can leave home after all these three things have happened:
+    1. They have had no fever for at least 72 hours (that is three full days of no fever without the use medicine that reduces fevers)
+    2. other symptoms have improved (for example, when their cough or shortness of breath have improved)
+    3. at least 7 days have passed since their symptoms first appeared
+* If they will be tested to determine if they are still contagious, they can leave home after these three things have happened:
+    1. They no longer have a fever (without the use medicine that reduces fevers)
+    2. other symptoms have improved (for example, when their cough or shortness of breath have improved)
+    3. They received two negative tests in a row, 24 hours apart. Their doctor will follow CDC guidelines.
+`,
+  protection: `
+### KNOW HOW IT SPREADS
+
+* There is currently no vaccine to prevent coronavirus disease 2019 (COVID-19).
+* The best way to prevent illness is to avoid being exposed to this virus.
+* The virus is thought to spread mainly from person-to-person.
+* Between people who are in close contact with one another (within about 6 feet).
+* Through respiratory droplets produced when an infected person coughs or sneezes.
+* These droplets can land in the mouths or noses of people who are nearby or possibly be inhaled into the lungs.
+
+### TAKE STEPS TO PROTECT YOURSELF
+
+1. Wash hands frequently with hot water and soap for 20 seconds
+2. Social distancing
+3. Clean and disinfect surfaces that can be contaminated
+4. Wear face masks - Here is how to make your own!
+
+### PLAN & COPE
+* Plan and make decisions in advance of an illness.
+* Know how to protect and support the children in your care.
+* Find ways to cope with stress that will make you, your loved ones, and your community stronger.
+`,
+  sick: `
+### SELF-CHECKER
+
+A guide to help you make decisions and seek appropriate medical care
+
+### MONITOR YOUR SYMPTOMS
+
+* Common symptoms of COVID-19 include fever and cough. Trouble breathing is a more serious symptom that means you should get medical attention.
+* If you are having trouble breathing, seek medical attention, but call first.
+* Wear a facemask: If available, put on a facemask before you enter the building. If you can’t put on a facemask, cover your coughs and sneezes. Try to stay at least 2 meters away from other people.
+* Follow care instructions from your healthcare provider and local health department: Your local health authorities may give instructions on checking your symptoms and reporting information.
+
+### STAY HOME, EXCEPT TO GET MEDICAL CARE
+
+* Most people with COVID-19 have mild illness and are able to recover at home without medical care. Do not leave your home, except to get medical care. Do not visit public areas.
+* Call before you get medical care. Be sure to get care if you have trouble breathing, or have any other emergency warning signs, or if you think it is an emergency.
+* Avoid public transportation: Avoid using public transportation, ride-sharing, or taxis.
+* Separate yourself from other people in your home, this is known as home isolation. You should stay in a specific "sick area" if possible. Use a separate bathroom, if available.
+* Wear a facemask, if available, when you are around other people (including before you enter a healthcare provider’s office).
+* Get rest and stay hydrated.
+* Call xxx and notify the dispatch personnel that you have or may have COVID-19.
+
+WHEN TO SEEK MEDICAL ATTENTION
+If you develop emergency warning signs for COVID-19, get medical attention immediately. Emergency warning signs include, but are not limited to:
+* Trouble breathing
+* Persistent pain or pressure in the chest
+* New confusion or inability to arouse
+* Bluish lips or face
+* Severe, constant dizziness or lightheadedness
+* Slurred speech
+
+If you develop emergency warning signs, call xxx.
+
+TAKE STEPS TO PROTECT OTHERS
+
+Cover your coughs and sneezes
+* Cover: Cover your mouth and nose with a tissue when you cough or sneeze. If you don’t have a tissue, cough or sneeze into your elbow.
+* Dispose: Throw used tissues in a lined trash can.
+Clean your hands often
+* Wash hands: Wash your hands often with soap and water for at least 20 seconds. This is especially important after blowing your nose, coughing, or sneezing; going to the bathroom; and before eating or preparing food.
+* Hand sanitizer: If soap and water are not available, use an alcohol-based hand sanitizer with at least 60% alcohol, covering all surfaces of your hands and rubbing them together until they feel dry.
+* Soap and water: Soap and water are the best option, especially if hands are visibly dirty.
+* Avoid touching: Avoid touching your eyes, nose, and mouth with unwashed hands.
+Avoid sharing personal household items and clean frequently
+* Do not share: Do not share dishes, drinking glasses, cups, eating utensils, towels, or bedding with other people in your home.
+* Wash thoroughly after use: After using these items, wash them thoroughly with soap and water or put in the dishwasher.
+* Clean and disinfect areas that may have blood, stool, or body fluids on them.
+* Household cleaners and disinfectants: Clean the area or item with soap and water or another detergent if it is dirty. Then, use a household disinfectant.
+
+GETTING HELP
+    * Use this tool to help you make decisions and seek appropriate medical care
+* If you need to talk to someone, call these hotlines managed by the Ministry of Health and Telecom
+* You can ask questions using self-service tools on Telegram, Whatsapp or SMS
+* You can pre-recorded information about COVID-19 by calling 8335 or 952
+`
 };
--- src/styles/AppContainer.js
+++ src/styles/AppContainer.js
@@ -1,7 +1,7 @@
 import styled from "vue-styled-components";
 
 export const AppContainer = styled.div`
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Roboto, Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
