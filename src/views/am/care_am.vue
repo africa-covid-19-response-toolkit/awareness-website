774d1239209aac09f37ff87bc6a1181fb2686c16
faithwins <yosef.semegn@gmail.com>
2020-04-10 01:20:03 -0400

    -adjusting routing and localization

--- src/views/am/care_am.vue
+++ src/views/am/care_am.vue
@@ -4,13 +4,13 @@
       <Header :imgSrc="imgSrc" />
     </template>
     <template v-slot:Body>
-      <p>{{ $t("sick") }}</p>
+      <p>{{ $t("care") }}</p>
     </template>
   </Details>
 </template>
 <script>
-import Details from "../components/details";
-import Header from "../components/header";
+import Details from "../../components/details";
+import Header from "../../components/header";
 export default {
   components: {
     Details,
@@ -18,7 +18,7 @@ export default {
   },
   data() {
     return {
-      imgSrc: `${process.env.BASE_URL}img/what-to-do-if-head.svg`
+      imgSrc: `${process.env.BASE_URL}img/how-to-care-head.svg`
     };
   }
 };
---
+++ src/views/am/what_am.vue
@@ -0,0 +1,25 @@
+<template>
+  <Details>
+    <template v-slot:Header>
+      <Header :imgSrc="imgSrc" />
+    </template>
+    <template v-slot:Body>
+      <p>{{ $t("what") }}</p>
+    </template>
+  </Details>
+</template>
+<script>
+import Details from "../../components/details";
+import Header from "../../components/header";
+export default {
+  components: {
+    Details,
+    Header
+  },
+  data() {
+    return {
+      imgSrc: `${process.env.BASE_URL}img/what-is-covid-head.svg`
+    };
+  }
+};
+</script>
