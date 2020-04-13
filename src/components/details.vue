c13de817893b312a659f02e73b4d79b4696c95e6
faithwins <yosef.semegn@gmail.com>
2020-04-13 13:19:26 -0400

    Use home.svg and minor ui change on home link

--- src/components/details.vue
+++ src/components/details.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
-    <router-link :to="home" class="float-left my-2">
-      &lt; {{ $t("back") }}
-    </router-link>
-    <div><slot name="Header"></slot></div>
+    <div class="header">
+      <router-link :to="home" class="float-left my-2 homeLink">
+        <!--&lt; {{ $t("back") }}-->
+        <b-img :src="backImgSrc" style="height: 20px" fluid alt=""></b-img>
+      </router-link>
+      <slot name="Header"></slot>
+    </div>
     <slot name="Body"></slot>
   </div>
 </template>
@@ -12,9 +15,17 @@ import i18n from "../plugins/i18n";
 export default {
   data() {
     return {
-      backImgSrc: `${process.env.BASE_URL}img/back-btn.svg`,
+      backImgSrc: `${process.env.BASE_URL}img/home.svg`,
       home: `/${i18n.locale}`
     };
   }
 };
 </script>
+<style scoped>
+.header {
+  background-color: #f8f8f8;
+}
+.homeLink {
+  padding-left: 10px;
+}
+</style>
